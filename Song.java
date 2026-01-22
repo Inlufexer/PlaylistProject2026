e7f9362e4b428b443920f20fb8bf692aaaf5e01a
Mira Mavis <mandmvoice@gmail.com>
2026-01-22 10:09:58 -0800

    created the constructor for the song class, like is initially set to false

--- Song.java
+++ Song.java
@@ -17,7 +17,16 @@ public class Song {
      * Constructor-- what information needs to be given to make a Song?
      * How will you handle 'liked' songs? It makes sense for a Song not be 'liked' by default
      */
-    
+
+    public Song (String newTitle, String newArtist, int newMinute, int newSecond){
+        like = false;
+        title = newTitle;
+        artist = newArtist;
+        minutes = newMinute;
+        seconds = newSecond;
+
+    }
+
 
 
 
